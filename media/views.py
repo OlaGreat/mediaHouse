f76a53b813e9c79363ee35dbbed07479eb44796a
OlaGreat <oladipupoolamilekan2@gmail.com>
2024-01-26 21:05:22 +0100

    update

--- media/views.py
+++ media/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
 from .models import Media
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
     media = Media.objects.all()
 
     return render(request, 'home.html', {'media': media})
+
+
+
+def about(request):
+
+    return render(request, 'about.html', {})
+
+
+def login_user(request):
+        
+        return render(request, 'login.html', {})
+
+    
+
+
+def logout_user(request):
+    pass
