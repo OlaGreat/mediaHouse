87644d3c126302c8f626fc0a357fd71b4a9ab70e
OlaGreat <oladipupoolamilekan2@gmail.com>
2024-01-28 08:39:48 +0100

    update

--- media/views.py
+++ media/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Media
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
@@ -16,11 +16,24 @@ def about(request):
 
 
 def login_user(request):
-        
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, (f"Welcome back {username}"))
+            return redirect('home')
+        else:
+            messages.success(request, ("You've been logged out"))
+            return redirect('about.html')
+    else:
         return render(request, 'login.html', {})
 
     
 
 
 def logout_user(request):
-    pass
+    logout(request)
+    return redirect('home')
