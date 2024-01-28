f12236fe46e6e2f5ea116a72728a8b1e42200698
OlaGreat <oladipupoolamilekan2@gmail.com>
2024-01-28 09:11:19 +0100

    update

--- media/views.py
+++ media/views.py
@@ -37,3 +37,8 @@ def login_user(request):
 def logout_user(request):
     logout(request)
     return redirect('home')
+
+
+def register_user(request):
+    
+    return render(request, 'register.html', {})
