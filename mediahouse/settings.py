57466824a317f27ce627277e7c13cde641ac1dda
OlaGreat <oladipupoolamilekan2@gmail.com>
2024-02-03 23:11:57 +0100

    update

--- mediahouse/settings.py
+++ mediahouse/settings.py
@@ -29,13 +29,13 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-e+f@@+1xc7u^0
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'RENDER' not in os.environ
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 # ----------------------------------------------------------------------------------------------------------
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+# RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+# if RENDER_EXTERNAL_HOSTNAME:
+#     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
