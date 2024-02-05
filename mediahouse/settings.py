7c0a2b385f23b44ca4be3b107d0483f8090dea4b
OlaGreat <oladipupoolamilekan2@gmail.com>
2024-02-06 00:52:24 +0100

    update

--- mediahouse/settings.py
+++ mediahouse/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # ----------------------------------------------------------------------------------------------------------------------------------
 SECRET_KEY = os.environ.get('SECRET_KEY', default='my secret key')
 
-x# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'RENDER' not in os.environ
 DEBUG = True
 
